1d62606404042e5b6b5e07fba23328c138d431e3
Eric Oestrich <eric@oestrich.org>
2017-10-05 16:22:11 -0400

    Let ExUnit start the formatter and pass in options
    Delegate to the CLIFormatter by default

--- lib/radex/application.ex
+++ lib/radex/application.ex
@@ -8,7 +8,6 @@ defmodule Radex.Application do
   def start(_type, _args) do
     # List all child processes to be supervised
     children = [
-      {Radex.Formatter, []},
       {Radex.Metadata, []},
     ]
 
--- lib/radex/formatter.ex
+++ lib/radex/formatter.ex
@@ -7,25 +7,21 @@ defmodule Radex.Formatter do
 
   use GenServer
 
-  @doc false
-  def start_link(_) do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
-  end
-
   #
   # Server
   #
 
-  def init(_) do
-    {:ok, %{}}
+  def init(opts) do
+    ExUnit.CLIFormatter.init(opts)
   end
 
-  def handle_cast({:suite_finished, _run_us, _load_us}, state) do
-    IO.puts "Writing documentation"
-    {:noreply, state}
+  def handle_cast(args = {:suite_finished, _run_us, _load_us}, state) do
+    IO.puts "\n"
+    IO.write "Writing documentation"
+    ExUnit.CLIFormatter.handle_cast(args, state)
   end
 
-  def handle_cast(_, state) do
-    {:noreply, state}
+  def handle_cast(args, state) do
+    ExUnit.CLIFormatter.handle_cast(args, state)
   end
 end
